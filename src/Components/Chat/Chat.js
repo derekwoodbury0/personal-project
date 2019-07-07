c6e6f1dc810f44e4a7cf2dd573b086ef6c53c7a9
Derek <Derek@Dereks-MacBook-Pro.local>
2019-07-07 00:21:37 -0600

    added handling so only logged in users can use chat

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -2,97 +2,112 @@ import React, { Component } from 'react'
 import './Chat.css'
 import axios from 'axios'
 import openSocket from 'socket.io-client'
+import { connect } from 'react-redux'
 
 const socket = openSocket('http://10.0.0.164:4001')
 
 class Chat extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            chat: false,
-            message: '',
-            messages: []
-        }
+    this.state = {
+      chat: false,
+      message: '',
+      messages: []
     }
-    
-    componentDidMount() {
+  }
+
+  componentDidMount() {
     this.subscribeToTimer()
-    }
+  }
 
-    toggleChat = () => {
-        this.setState ({ chat: !this.state.chat})
-      }
-    
-      handleChange = (e) => {
-        let { name, value } = e.target
-    
-        this.setState ({ [name]: value })
-      }
-    
-      handleClick = () => {
-        if (this.state.message) {
-            let { message } = this.state
-            axios.post('/api/chattext', {message})
-            this.sendMessage(message)
-            this.setState ({ message: '' })
-        }
-      }
-    
-      subscribeToTimer(cb){
-        socket.emit('subscribeToTimer', 1000)
-        socket.on('messages', cb = (messages) => {
-            this.setState ({ messages })
-        })
-      }
-    
-      receiveMessage(){
-          console.log('receiveMessage')
-          socket.on('emittedMessage', receivedMessages => {
-              console.log('emittedMessage handler invoked', receivedMessages)
-              this.setState ({ messages: receivedMessages})
-          })    
-        }
-    
-      sendMessage(message){
-          socket.emit('sendMessage', message)
-      }
-    
-    render() {
-        return (
-            <div>
-            <div onClick={this.toggleChat} className="live-chat-link">
-                <i className="far fa-comment-alt" ></i>
-            </div>
-            {this.state.chat &&
-            <div className="live-chat">
-                <div className="live-chat-conversation-box">
-                  <div className="live-chat-conversation-box-inner">
-                    { this.state.messages === [] ?
-                    null
-                    :
-                    this.state.messages.map((message, index) => {
-                        return (
-                            <h2 key ={index} style={{color: 'black'}}>{message}</h2>
-                        )
-                    })
-                    }
-                  </div>
-                </div>
-                <div className="live-chat-input-box">
-                    <input 
-                        placeholder="type question" 
-                        name="message"
-                        onChange={this.handleChange}
-                        value={this.state.message}    
-                    />
-                    <button onClick={this.handleClick}>Send</button>
-                </div>
-            </div>
-            }
-          </div>
-        )
+  toggleChat = () => {
+    this.setState({ chat: !this.state.chat })
+  }
+
+  handleChange = (e) => {
+    let { name, value } = e.target
+
+    this.setState({ [name]: value })
+  }
+
+  handleClick = () => {
+    if (this.state.message) {
+      let { message } = this.state
+      axios.post('/api/chattext', { message })
+      this.sendMessage(message)
+      this.setState({ message: '' })
     }
+  }
+
+  subscribeToTimer(cb) {
+    socket.emit('subscribeToTimer', 1000)
+    socket.on('messages', cb = (messages) => {
+      this.setState({ messages })
+    })
+  }
+
+  receiveMessage() {
+    console.log('receiveMessage')
+    socket.on('emittedMessage', receivedMessages => {
+      console.log('emittedMessage handler invoked', receivedMessages)
+      this.setState({ messages: receivedMessages })
+    })
+  }
+
+  sendMessage(message) {
+    socket.emit('sendMessage', message)
+  }
+
+  render() {
+    return (
+      <div>
+        <div onClick={this.toggleChat} className="live-chat-link">
+          <i className="far fa-comment-alt" ></i>
+        </div>
+        {this.state.chat &&
+          <div className="live-chat">
+            <div className="live-chat-conversation-box">
+              <div className="live-chat-conversation-box-inner">
+                {this.state.messages !== [] && this.props.user ?
+                  this.state.messages.map((message, index) => {
+                    return (
+                      <h2 key={index} style={{ color: 'black' }}>{message}</h2>
+                    )
+                  })
+                  :
+                  <h2 style={{ color: 'black' }}>Please Log In To Chat</h2>
+                }
+              </div>
+            </div>
+            <div className="live-chat-input-box">
+              {this.props.user ?
+              <input
+                placeholder="type question"
+                name="message"
+                onChange={this.handleChange}
+                value={this.state.message}
+              />
+              :
+              <input
+                placeholder="Please Log In To Chat"
+                style={{background: 'lightgray', color: 'black'}}
+                disabled
+              />
+              }
+              <button onClick={this.handleClick}>Send</button>
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
 }
 
-export default Chat
+const mapStateToProps = state => {
+  return {
+    user: state.userReducer.data,
+  }
+}
+
+export default connect(mapStateToProps)(Chat)
